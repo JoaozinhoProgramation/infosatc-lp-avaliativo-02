a923a6e73be1c69d1396ccc278c3eb9a41d8b67e
JoaozinhoProgramation <joao.243619@alunosatc.edu.br>
2021-09-06 17:17:08 -0300

    Create Avaliacion7d.py

---
+++ Avaliacion7d.py
@@ -0,0 +1,11 @@
+
+
+Filmes = ["infinite","Jungle Cruise",]
+Jogos = ["Hitman","Crash"]
+Livros = ["Você sabia","mindset"]
+Esporte = ["Futebol","Basquete"]
+
+
+Esporte.remove("Futebol")
+
+print(D)
