5519cf689f5ea5463b3463f85a49a19c30274a64
JoaozinhoProgramation <joao.243619@alunosatc.edu.br>
2021-09-06 17:17:16 -0300

    Create Avaliacion8.py

---
+++ Avaliacion8.py
@@ -0,0 +1,7 @@
+pcgamer = ["Intel Core i5","GeForce RTX 2080Ti","Corsair Vengeance LPX (2x 4 GB)",
+           "Seagate Barracuda 1 TB","Asus EX-B460M-V5","Corsair CV550 550 Watts","Gabinete Cooler Master"]
+
+pcgamer.remove("Gabinete Cooler Master")
+pcgamer.remove("GeForce RTX 2080Ti")
+
+print(pcgamer)
