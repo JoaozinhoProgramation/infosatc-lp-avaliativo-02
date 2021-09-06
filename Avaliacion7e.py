fa0afdd326b13e058d5a74f391411a69043e8a09
JoaozinhoProgramation <joao.243619@alunosatc.edu.br>
2021-09-06 17:17:12 -0300

    Create Avaliacion7e.py

---
+++ Avaliacion7e.py
@@ -0,0 +1,12 @@
+
+
+Filmes = ["infinite","Jungle Cruise",]
+Jogos = ["Hitman","Crash"]
+Livros = ["Você sabia","mindset"]
+Esporte = ["Futebol","Basquete"]
+Disciplinas =["Matemática","História"]
+
+
+Lista = Filmes.copy(),Jogos.copy(),Livros.copy(),Esporte.copy(),Disciplinas.copy()
+
+print(ABCD)
