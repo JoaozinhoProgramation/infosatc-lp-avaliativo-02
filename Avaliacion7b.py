8ceb9a3402705db7784e1e495b05778effb41b96
JoaozinhoProgramation <joao.243619@alunosatc.edu.br>
2021-09-06 17:17:02 -0300

    Create Avaliacion7b.py

---
+++ Avaliacion7b.py
@@ -0,0 +1,9 @@
+
+Filmes = ["infinite","Jungle Cruise",]
+Jogos = ["Hitman","Crash"]
+Livros = ["Você sabia","mindset"]
+Esporte = ["Futebol","Basquete"]
+
+Lista = Filmes.copy(),Jogos.copy(),Livros.copy(),Esporte.copy()
+
+print(ABCD)
